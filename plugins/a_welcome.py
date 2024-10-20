ccbf91540f96146fb1255654a7b24154c8450d70
Dvis <gokhaledurgesh4@gmail.com>
2024-10-20 19:51:58 +0530

    Auto Fixes

--- plugins/a_welcome.py
+++ plugins/a_welcome.py
@@ -164,8 +164,8 @@ async def greet_new_members(_, member: ChatMemberUpdated):
 
         # Add the modified condition here
         if member.new_chat_member and not member.old_chat_member:
-            welcome_text = f"""{user.mention}** ʜᴏᴡ ᴀʀᴇ ʏᴏᴜ??**"""
-            await asyncio.sleep(3)
+            welcome_text = f"""{user.mention}** ᴡʜᴀᴛ's ᴜᴘ??**"""
+            await asyncio.sleep(1)
             await userbot.send_message(chat_id, text=welcome_text)
     except Exception as e:
         return
